52e8177e46d485e15c3e1d1c8a704feabfd34099
Kojo Kwakye <kodeon6@gmail.com>
2024-08-27 19:37:21 +0100

    Create user and post model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :posts
+  has_many :comments
+  has_many :follower_relationships, foreign_key: :followed_id, class_name: "Follow"
+  has_many :followers, through: :follower_relationships, source: :follower
+  has_many :followed_relationships, foreign_key: :follower_id, class_name: "Follow"
+  has_many :followeds, through: :followed_relationships, source: :followed
+
+  def follow(user)
+    followed_relationships.create(followed: user)
+  end
+
+  def unfollow(user)
+    followed_relationships.find_by(followed: user).destroy
+  end
+
+  def following?(user)
+    followeds.include?(user)
+  end
 end
